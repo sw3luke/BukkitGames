e1573510d6ddbf4e54cecd6fe0fd97b6d23e4a86
ftbastler <user@server.end>
2012-10-28 11:37:44 +0100

    Fixed commit

---
+++ HG/src/utilities/BGFeast.java
@@ -0,0 +1,238 @@
+package utilities;
+
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import main.BGMain;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+public class BGFeast {
+
+	private static BGMain plugin;
+	private static Block mainBlock = null;
+	private static Integer radius = 8;
+	private static Logger log = BGMain.getPluginLogger();
+	private static Boolean spawned = false;
+	private static Chest[] chests = new Chest[8];
+	
+	public BGFeast(BGMain plugin) {	
+		BGFeast.plugin = plugin;
+	}
+	
+	public static void announceFeast(Integer time) {
+		if(mainBlock == null) {
+			do {
+				mainBlock = BGMain.getRandomLocation().subtract(0, 1, 0).getBlock();
+			} while (!plugin.inBorder(mainBlock.getLocation()));
+			mainBlock.setType(Material.NETHERRACK);
+			removeAbove(mainBlock);
+			createFeast(Material.SOUL_SAND);
+			spawned = true;
+		}
+		
+		String s = "";
+		if(time > 1)
+			s = "s";
+		
+		DecimalFormat df = new DecimalFormat("##.#");
+		BGChat.printInfoChat("Feast will spawn in " + time + " minute" + s + " at X: " + df.format(mainBlock.getLocation().getX()) + " | Y: " + df.format(mainBlock.getLocation().getY()) + " | Z: " + df.format(mainBlock.getLocation().getZ()));
+	}
+	
+	public static void spawnFeast() {
+		if(mainBlock == null)
+			announceFeast(0);
+		
+		DecimalFormat df = new DecimalFormat("##.#");
+		BGChat.printInfoChat("Feast spawned at X: " + df.format(mainBlock.getLocation().getX()) + " | Y: " + df.format(mainBlock.getLocation().getY()) + " | Z: " + df.format(mainBlock.getLocation().getZ()));
+		if(plugin.FEAST_CHESTS)
+			spawnChests();
+		
+		List<String> items = BGFiles.feastconf.getStringList("ITEMS");
+		for(String item : items) {
+			String[] oneitem = item.split(",");
+			Random r = new Random();
+			String itemid = oneitem[0];
+			Integer minamount = Integer.parseInt(oneitem[1]);
+			Integer maxamount = Integer.parseInt(oneitem[2]);
+			Integer amount = 0;
+			Boolean force = Boolean.parseBoolean(oneitem[3]);
+			Boolean spawn = force;
+			Integer id = null;
+			Short durability = null;
+			
+			if(!force)
+				spawn = r.nextBoolean();
+			
+			if(!spawn)
+				continue;
+			
+			if (item.contains(":")) {
+				String[] it = itemid.split(":");
+				id = Integer.parseInt(it[0]);
+				durability = Short.parseShort(it[1]);
+			} else {
+				id = Integer.parseInt(itemid);
+			}
+			
+			ItemStack i = new ItemStack(id, 1);
+			
+			if(durability != null)
+				i.setDurability(durability);
+			
+			if(oneitem.length == 6)
+				i.addUnsafeEnchantment(Enchantment.getById(Integer.parseInt(oneitem[4])), Integer.parseInt(oneitem[5]));
+						
+			Integer ra = radius;
+			
+			if(maxamount == minamount)
+				amount = maxamount;
+			else
+				amount = minamount + r.nextInt(maxamount - minamount + 1);
+			
+			if(plugin.FEAST_CHESTS) {				
+				while(amount > 0) {
+					Chest chest = chests[r.nextInt(8)];
+					Integer slot = r.nextInt(27);
+					int maxtry = 0;
+					while(chest.getInventory().getItem(slot) != null 
+							&& !chest.getInventory().getItem(slot).getType().equals(i.getType()) && maxtry < 1000)
+						slot = r.nextInt(27);
+					if(chest.getInventory().getItem(slot) != null)
+						i.setAmount(i.getAmount() + 1);
+					chest.getInventory().setItem(slot, i);
+					chest.update();
+					amount--;
+				}
+			} else {
+				Location c = mainBlock.getLocation();
+				c.add(-(ra/2) + r.nextInt(ra), 1, -(ra/2) + r.nextInt(ra));
+				while(amount > 0) {
+					Bukkit.getServer().getWorld("world").dropItemNaturally(c, i).setPickupDelay(20 * 5);
+					amount--;
+				}
+			}
+		}
+	}
+	
+	public static Boolean isFeastBlock(Block b) {		
+		if(!plugin.FEAST || !spawned)
+			return false;
+
+		if(b.getLocation().distance(mainBlock.getLocation()) <= radius + 3)
+			return true;
+
+		return false;
+	}
+	
+	private static void createFeast(Material m) {
+		Location loc = mainBlock.getLocation();
+		Integer r = radius;
+	               
+		log.info("Generating feast.");	    
+	    
+	    for (double x = -r; x <= r; x++) {
+	        for (double z = -r; z <= r; z++) {
+	        	Location l = new Location(Bukkit.getServer().getWorld("world"), loc.getX() + x, loc.getY(), loc.getZ() + z);
+	        	if(l.distance(loc) <= r && l.getBlock().getType() != Material.NETHERRACK) {
+	        		removeAbove(l.getBlock());
+	        		l.getBlock().setType(m);
+	        	}
+	        }
+	    }
+	}
+	
+	private static void spawnChests() {
+		Location loc = mainBlock.getLocation();
+		loc.add(-3, 1, -3);
+		Integer curchest = 0;
+		
+		//-2: new layer; -1: new row; 0: air; 1: block; 
+		// 2: chest; 3: enchanting table; 4: fence; 5 : no change
+		Integer[] co = {0, 0, 0, 0, 0, 0, 0, -1,
+				0, 0, 2, 1, 2, 0, 0, -1,
+				0, 2, 1, 1, 1, 2, 0, -1,
+				0, 1, 1, 1, 1, 1, 0, -1,
+				0, 2, 1, 1, 1, 2, 0, -1,
+				0, 0, 2, 1, 2, 0, 0, -1,
+				0, 0, 0, 0, 0, 0, 0, -2,
+				
+				0, 0, 0, 0, 0, 0, 0, -1,
+				0, 0, 0, 0, 0, 0, 0, -1,
+				0, 0, 0, 0, 0, 0, 0, -1,
+				0, 0, 0, 3, 0, 0, 0, -1,
+				0, 0, 0, 0, 0, 0, 0, -1,
+				0, 0, 0, 0, 0, 0, 0, -1,
+				0, 0, 0, 0, 0, 0, 0, -2,};
+		
+		for(Integer i : co) {
+			Material m = Material.AIR;
+			switch (i) {
+			case 0:
+				m = Material.AIR;
+				break;
+			case 1:
+				m = Material.OBSIDIAN;
+				break;
+			case 2:
+				m = Material.CHEST;
+				break;
+			case 3:
+				m = Material.ENCHANTMENT_TABLE;
+				break;
+			case 4:
+				m = Material.FENCE;
+				break;
+			case 5:
+				m = null;
+				break;
+			case -1:
+				break;
+			case -2:
+				break;
+			default:
+				log.warning("Illegal integer found while creating Chests at Feast: " + i.toString());
+				break;
+			}
+			
+			if(i == -1) {
+				loc.add(0, 0, 1);
+				loc.subtract(7, 0, 0);
+			} else if(i == -2) {
+				loc.add(0, 1, 0);
+				loc.subtract(7, 0, 6);
+			} else {
+				if (m == null){
+					loc.add(1, 0, 0);
+				}else {
+					loc.getBlock().setType(m);
+					if(m == Material.CHEST) {
+						chests[curchest] = (Chest) loc.getBlock().getState();
+						if(curchest < 8) curchest++;
+					}
+					loc.add(1, 0, 0);
+				}
+			}
+		}
+	}
+	
+	public static void removeAbove(Block block) {
+		Location loc = block.getLocation();
+		loc.setY(loc.getY()+1);
+		Block newBlock = Bukkit.getServer().getWorld("world").getBlockAt(loc);
+		while(loc.getY() < Bukkit.getServer().getWorld("world").getMaxHeight()) {
+			newBlock.setType(Material.AIR);
+			loc.setY(loc.getY()+1);
+			newBlock = Bukkit.getServer().getWorld("world").getBlockAt(loc);
+		}
+	}
+	
+}
