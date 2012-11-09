3762f90658395c8b579dffbae8b9646c7a536f3c
Christoph Hackenberger <chackenberger@me.com>
2012-11-09 22:55:02 +0100

    Fixed Bug!

--- HG/src/utilities/BGSign.java
+++ HG/src/utilities/BGSign.java
@@ -25,39 +25,43 @@ public class BGSign extends JavaPlugin{
 	
 	public void createSign (Location loc, String firstLine, String secondLine, String thirdLine, String fourthLine) {
 		
-		String fline = firstLine;
-		String sline = secondLine;
-		String tline = thirdLine;
-		String foline = fourthLine;
+		try{
+			String fline = firstLine;
+			String sline = secondLine;
+			String tline = thirdLine;
+			String foline = fourthLine;
 		
-		if(fline != null && fline.length() > 15) {
-			fline = fline.substring(0, 15);
+			if(fline != null && fline.length() > 15) {
+				fline = fline.substring(0, 15);
+			}
+			if(sline != null && sline.length() > 15) {
+				sline = sline.substring(0, 15);
+			}
+			if(tline != null && tline.length() > 15) {
+				tline = tline.substring(0, 15);
+			}
+			if(foline != null && foline.length() > 15) {
+				foline = foline.substring(0, 15);
+			}
+		
+			Block block = Bukkit.getWorld("world").getBlockAt(loc);
+			block.setType(Material.SIGN_POST);
+			Sign sign = (Sign) block.getState();
+		
+			if(fline != null)
+				sign.setLine(0, fline);
+			if(sline != null)
+				sign.setLine(1, sline);
+			if(tline != null)	
+				sign.setLine(2, tline);
+			if(foline != null)
+				sign.setLine(3, foline);
+		
+			sign.update(true);
+		
+			signs.add(sign);
+		}catch (Exception e) {
+			
 		}
-		if(sline != null && sline.length() > 15) {
-			sline = sline.substring(0, 15);
-		}
-		if(tline != null && tline.length() > 15) {
-			tline = tline.substring(0, 15);
-		}
-		if(foline != null && foline.length() > 15) {
-			foline = foline.substring(0, 15);
-		}
-		
-		Block block = Bukkit.getWorld("world").getBlockAt(loc);
-		block.setType(Material.SIGN_POST);
-		Sign sign = (Sign) block.getState();
-		
-		if(fline != null)
-			sign.setLine(0, fline);
-		if(sline != null)
-			sign.setLine(1, sline);
-		if(tline != null)	
-			sign.setLine(2, tline);
-		if(foline != null)
-			sign.setLine(3, foline);
-		
-		sign.update(true);
-		
-		signs.add(sign);
 	}
 }
