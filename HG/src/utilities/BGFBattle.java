cca7f0474cab43941f6be5dd21b467e45e4a63b2
Galodystic <chackenberger@me.com>
2012-12-01 23:46:34 +0100

    Fixed Bug!

--- HG/src/utilities/BGFBattle.java
+++ HG/src/utilities/BGFBattle.java
@@ -13,7 +13,6 @@ import main.BGMain;
 
 public class BGFBattle {
 
-	@SuppressWarnings("unused")
 	private static BGMain plugin;
 	
 	private static Block mainBlock;
@@ -29,7 +28,9 @@ public class BGFBattle {
 	
 	public static void createBattle() {
 		
-		mainBlock = BGMain.getRandomLocation().subtract(0, 1, 0).getBlock();
+		do{
+			mainBlock = BGMain.getRandomLocation().subtract(0, 1, 0).getBlock();
+		}while(!plugin.inBorder(mainBlock.getLocation()));
 		
 		Location loc = mainBlock.getLocation();
 		
